da5e0fdbcb4c9c7648d06579c816b7e6fa89b1fe
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-02-18 18:38:07 +0300

    add function greeting

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const time = document.querySelector(".time");
 const date = document.querySelector(".date");
+const greeting = document.querySelector(".greeting");
 
+// Приветствие
+function showGreeting() {
+  function getTimeOfDay() {
+    const hours = new Date().getHours();
+    if (hours >= 6 && hours < 12) {
+      return "morning";
+    }
+    if (hours >= 12 && hours < 18) {
+      return "afternoon";
+    }
+    if (hours >= 18 && hours <= 23) {
+      return "evening";
+    }
+    if (hours >= 0 && hours < 6) {
+      return "night";
+    }
+  }
+
+  const timeOfDay = getTimeOfDay();
+  const greetingText = `Good ${timeOfDay}`;
+  greeting.textContent = greetingText;
+}
+
+// Дата
 function showDate() {
   const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
   const currentDate = new Date().toLocaleDateString("en-US", options);
   date.textContent = currentDate;
 }
 
+//Время
 function showTime() {
   const currentTime = new Date().toLocaleTimeString();
   time.textContent = currentTime;
   showDate();
+  showGreeting();
   setTimeout(showTime, 1000);
 }
 
