62426dd1ed3166fd50b8d34559ca666f61710cf3
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-02-25 21:15:27 +0300

    fixes: пути sounds

--- scripts/playList.js
+++ scripts/playList.js
@@ -1,22 +1,22 @@
 const playList = [
   {
     title: "Aqua Caelestis",
-    src: "../assets/sounds/AquaCaelestis.mp3",
+    src: "./assets/sounds/AquaCaelestis.mp3",
     duration: "00:58",
   },
   {
     title: "River Flows In You",
-    src: "../assets/sounds/RiverFlowsInYou.mp3",
+    src: "./assets/sounds/RiverFlowsInYou.mp3",
     duration: "03:50",
   },
   {
     title: "Ennio Morricone",
-    src: "../assets/sounds/EnnioMorricone.mp3",
+    src: "./assets/sounds/EnnioMorricone.mp3",
     duration: "01:37",
   },
   {
     title: "Summer Wind",
-    src: "../assets/sounds/SummerWind.mp3",
+    src: "./assets/sounds/SummerWind.mp3",
     duration: "01:50",
   },
 ];
